772b383a0f4473fb5b6fcd84325442ae56ab4833
Matorry <rodri.martinceron@gmail.com>
2024-01-31 22:31:24 +0100

    Add Mongoose configuration for database connection

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -1,12 +1,15 @@
-const Hapi = require("@hapi/hapi");
-
-const server = Hapi.server({
-  port: process.env.PORT || 3000,
-});
+import Hapi from "@hapi/hapi";
+import { dbConnect } from "./db/db.connect.js";
 
 const init = async () => {
+  await dbConnect();
+
+  const server = new Hapi.Server({
+    port: 3000,
+    host: "localhost",
+  });
   await server.start();
-  console.log(`Server running on ${server.info.uri}`);
+  console.log(`Server running on: ${server.info.uri}`);
 };
 
 process.on("unhandledRejection", (err) => {
