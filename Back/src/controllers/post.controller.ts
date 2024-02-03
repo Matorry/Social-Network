6ea751b193946fa634551da535a95d4d2e0efc0e
Matorry <rodri.martinceron@gmail.com>
2024-02-03 19:49:48 +0100

    Add post controller with createPost functionality

---
+++ Back/src/controllers/post.controller.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+import { Request, ResponseToolkit } from "@hapi/hapi";
+import { Post, PostNoId } from "../entities/post.js";
+import { PostMongoRepository } from "../repository/post.mongo.repository.js";
+import { UserMongoRepository } from "../repository/user.mongo.repository.js";
+import { Controller } from "./controller.js";
+
+export class PostController extends Controller<Post> {
+  constructor(
+    protected postRepo: PostMongoRepository,
+    userRepo: UserMongoRepository,
+  ) {
+    super(postRepo);
+  }
+
+  protected userRepo = new UserMongoRepository();
+
+  async createPost(request: Request, response: ResponseToolkit) {
+    try {
+      const newPost = request.payload as Post;
+      const author = await this.userRepo.get(newPost.author.id);
+      newPost.author = author;
+      newPost.date = new Date();
+      const post = await this.repo.post(request.payload as PostNoId);
+      return response.response(post).code(201);
+    } catch (error) {
+      return response.response({ error: "Internal Server Error" }).code(500);
+    }
+  }
+}
