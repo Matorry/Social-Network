b5c8986ce3f10f79aea867c9274e143211035fde
Matorry <rodri.martinceron@gmail.com>
2024-02-03 20:21:30 +0100

    Add ApiPostRepository

---
+++ Front/src/services/post.repository.ts
@@ -0,0 +1,27 @@
+import { Post, PostNoId } from '../models/post';
+import { Repository } from './repository';
+
+export class ApiPostRepository implements Repository<Post> {
+  urlBase: string;
+
+  constructor(urlBase: string) {
+    this.urlBase = urlBase;
+  }
+
+  async create(item: PostNoId, token: string): Promise<Post> {
+    const response = await fetch(`${this.urlBase}/post/create`, {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok)
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+
+    const data = await response.json();
+    return data;
+  }
+}
