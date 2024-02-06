10ed1110897ce39357df26799e5f5cdc3ece3503
Matorry <rodri.martinceron@gmail.com>
2024-02-06 11:39:08 +0100

    Update app

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -1,12 +1,16 @@
 import Hapi from "@hapi/hapi";
+import { CommentController } from "./controllers/comment.controller.js";
 import { PostController } from "./controllers/post.controller.js";
 import { UsersController } from "./controllers/user.controller.js";
 import { dbConnect } from "./db/db.connect.js";
+import { Comment } from "./entities/comment.js";
 import { Post } from "./entities/post.js";
 import { User } from "./entities/user.js";
+import { CommentMongoRepository } from "./repository/comment.mongo.repository.js";
 import { PostMongoRepository } from "./repository/post.mongo.repository.js";
 import { Repository } from "./repository/repository.js";
 import { UserMongoRepository } from "./repository/user.mongo.repository.js";
+import { CommentRouter } from "./routers/comment.router.js";
 import { PostRouter } from "./routers/post.router.js";
 import { UsersRouter } from "./routers/user.router.js";
 
@@ -29,8 +33,13 @@ const init = async () => {
   const postController = new PostController(postRepo);
   const postRoutes = new PostRouter(postController);
 
+  const commentRepo: Repository<Comment> = new CommentMongoRepository();
+  const commentController = new CommentController(commentRepo);
+  const commentRoutes = new CommentRouter(commentController);
+
   await usersRoutes.configureRoutes(server);
   await postRoutes.configureRoutes(server);
+  await commentRoutes.configureRoutes(server);
   await server.start();
   console.log(`Server running on: ${server.info.uri}`);
 };
