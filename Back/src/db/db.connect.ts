b08e92acb560ab0936177754d57fdb97a71e5f99
Matorry <rodri.martinceron@gmail.com>
2024-02-07 19:06:36 +0100

    Update db connect

--- Back/src/db/db.connect.ts
+++ Back/src/db/db.connect.ts
@@ -5,8 +5,10 @@ export const dbConnect = async () => {
   try {
     const user = process.env.DB_USER;
     const passwd = process.env.DB_PASSWD;
-    const uri = `mongodb+srv://${user}:${passwd}@cluster0.kwgkdev.mongodb.net/simplesocialnetwork?retryWrites=true&w=majority`;
+    const clusterSubdomain = process.env.DB_SUBDOMAIN;
+    const uri = `mongodb+srv://${user}:${passwd}@cluster0.${clusterSubdomain}.mongodb.net/simplesocialnetwork?retryWrites=true&w=majority`;
     await mongoose.connect(uri);
+
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error}`);
